5c2a450ae02f9aa2650cdf2c01a845174ae073a1
aristantotristan <128369786+aristantotristan@users.noreply.github.com>
2025-11-24 23:41:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,6 @@
 const SUPABASE_URL = 'https://khamzxkrvmnjhrgdqbkg.supabase.co'; 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoYW16eGtydm1uamhyZ2RxYmtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NDg2MzcsImV4cCI6MjA3OTUyNDYzN30.SYZTZA3rxaE-kwFuKLlzkol_mLuwjYmVudGCN0imAM8'; 
 
-// Inisialisasi Klien Supabase di sini
+// Inisialisasi Klien Supabase (FIXED: Menggunakan window.supabase)
+// Klien ini akan digunakan oleh SEMUA file JS lainnya
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
