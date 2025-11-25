36613dfbaaceabd2ea1a72f6b029131c76dc2a9f
aristantotristan <128369786+aristantotristan@users.noreply.github.com>
2025-11-25 11:40:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 // =======================================================
-// config.js: KREDENSIAL SUPABASE FINAL
+// config.js: KREDENSIAL SUPABASE FINAL (Digunakan oleh semua fetch script)
 // =======================================================
 const SUPABASE_URL = 'https://khamzxkrvmnjhrgdqbkg.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoYW16eGtydm1uamhyZ2RxYmtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NDg2MzcsImV4cCI6MjA3OTUyNDYzN30.SYZTZA3rxaE-kwFuKLlzkol_mLuwjYmVudGCN0imAM8';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoYW16eGtydm1uamhyZ2RxYmtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NDg2MzcsImV4cCI6MjA3OTUyNDYzN30.SYZTZA3rxaE-kwFuKLlzkol_mLuwjYmVutGCN0imAM8'; // GANTI INI DENGAN KEY ASLI ANDA JIKA BERBEDA
+
+// Inisialisasi Klien Supabase (Diakses oleh semua file JS lain)
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
