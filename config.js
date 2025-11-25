0ee2068ae37afb1832fb146c5819451ee2a7b6ff
aristantotristan <128369786+aristantotristan@users.noreply.github.com>
2025-11-25 15:38:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-// =======================================================
-// config.js: KREDENSIAL SUPABASE FINAL (Digunakan oleh semua fetch script)
-// =======================================================
-const SUPABASE_URL = 'https://khamzxkrvmnjhrgdqbkg.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoYW16eGtydm1uamhyZ2RxYmtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NDg2MzcsImV4cCI6MjA3OTUyNDYzN30.SYZTZA3rxaE-kwFuKLlzkol_mLuwjYmVutGCN0imAM8'; // GANTI INI DENGAN KEY ASLI ANDA JIKA BERBEDA
+// config.js: KREDENSIAL SUPABASE FINAL
+const SUPABASE_URL = 'https://rojhcadtqfynlqzubftx.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvamhjYWR0cWZ5bmxxenViZnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMzYzNTksImV4cCI6MjA3NzcxMjM1OX0.XZElBWD-QdS8XVKex92VKUAlifC6BXqe3kGYPmZ1Mcs';
 
 // Inisialisasi Klien Supabase (Diakses oleh semua file JS lain)
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
