bc8cf51ec5b7f93a26cff9dbbc15290650b736c6
aristantotristan <128369786+aristantotristan@users.noreply.github.com>
2025-11-25 15:52:01 +0700

    Delete control_script.js

--- control_script.js
+++
@@ -1,7 +0,0 @@
-// control_script.js (Logic Log Downtime)
-
-const DOWNTIME_TABLE = 'connection_downtime_log';
-
-// ... (Semua fungsi fetchDowntimeLog, displayLogTable seperti kode sebelumnya) ...
-
-window.fetchDowntimeLog = fetchDowntimeLog;
