83b138c946d07d93606785ff369be54aaae579b0
aristantotristan <128369786+aristantotristan@users.noreply.github.com>
2025-11-25 15:51:15 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js: KREDENSIAL SUPABASE FINAL
-const SUPABASE_URL = 'https://rojhcadtqfynlqzubftx.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvamhjYWR0cWZ5bmxxenViZnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMzYzNTksImV4cCI6MjA3NzcxMjM1OX0.XZElBWD-QdS8XVKex92VKUAlifC6BXqe3kGYPmZ1Mcs';
-
-// Inisialisasi Klien Supabase (Diakses oleh semua file JS lain)
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
